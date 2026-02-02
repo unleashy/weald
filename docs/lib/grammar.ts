5f7a5ca1c41bf9f5cc44a286fbf313ba1522a74d
bnuuy.bsky.social <me@emmanuelle.dev>
2026-02-02 19:07:40 -0300

    Add grammar form tag

---
+++ docs/lib/grammar.ts
@@ -0,0 +1,127 @@
+export type Quantifier = "*" | "+" | "?";
+
+export type Term =
+  | { type: "nonterminal" | "terminal"; value: string; quantifier?: Quantifier }
+  | { type: "group"; value: Term[]; quantifier?: Quantifier };
+
+export function parseTerms(rule: string): Term[] {
+  return root(rule);
+}
+
+// Root: Seq End
+// Ws: [\s]*
+function root(s: string) {
+  let [result, end] = seq(s);
+  if (!/^\s*$/.test(end)) {
+    throw new SyntaxError(`expected end of input at ${JSON.stringify(end)}`);
+  }
+
+  return result;
+}
+
+// Seq: QuantifiableTerm*
+function seq(s: string) {
+  let terms: Term[] = [];
+
+  let qt;
+  while (([qt, s] = quantifiableTerm(s)) && qt) {
+    terms.push(qt);
+  }
+
+  return [terms, s] as const;
+}
+
+// QuantifiableTerm: Term Quantifier?
+function quantifiableTerm(s: string) {
+  let r = primaryTerm(s);
+
+  let value;
+  [value, s] = r;
+  if (!value) return r;
+
+  let r2 = quantifier(s);
+
+  let quant;
+  [quant, s] = r2;
+
+  return [{ ...value, quantifier: quant }, s] as const;
+}
+
+// Quantifier: Ws [*+?]
+function quantifier(s: string) {
+  let m = s.match(/^\s*([*+?])/);
+  if (!m) return [undefined, s] as const;
+
+  return [m[1] as Quantifier, s.slice(m[1].length)] as const;
+}
+
+// Term: Terminal | Nonterminal | Range | Group
+function primaryTerm(s: string) {
+  s = s.trimStart();
+  let r = terminal(s) ?? nonterminal(s) ?? range(s) ?? group(s);
+  if (r) {
+    return r;
+  } else {
+    return [undefined, s] as const;
+  }
+}
+
+// Terminal: Ws '```' | Ws '`' .+ '`' | Ws 'U+' [A-Fa-f0-9]+
+function terminal(s: string) {
+  if (s.startsWith("```")) {
+    return [{ type: "terminal", value: "`" }, s.slice(3)] as const;
+  } else if (s.startsWith("`")) {
+    let end = s.indexOf("`", 1);
+    if (end === -1) {
+      throw new SyntaxError(`unclosed string: ${JSON.stringify(s)}`);
+    }
+
+    return [{ type: "terminal", value: s.slice(1, end) }, s.slice(end + 1)] as const;
+  } else if (s.startsWith("U+")) {
+    let m = s.match(/^U\+[0-9A-Fa-f]{1,6}/);
+    if (!m) {
+      throw new SyntaxError(`bad unicode code point: ${JSON.stringify(s)}`);
+    }
+
+    return [{ type: "terminal", value: m[0] }, s.slice(m[0].length)] as const;
+  } else {
+    return undefined;
+  }
+}
+
+// Nonterminal: Ws [A-Za-z_] [A-Za-z0-9_]*
+function nonterminal(s: string) {
+  let m = s.match(/^[A-Za-z_][A-Za-z0-9_]*/);
+  if (!m) return undefined;
+
+  return [{ type: "nonterminal", value: m[0] }, s.slice(m[0].length)] as const;
+}
+
+// Range: Ws '[' .+ ']'
+function range(s: string) {
+  if (!s.startsWith("[")) return undefined;
+
+  let end = s.indexOf("]", 1);
+  if (end === -1) {
+    throw new SyntaxError(`unclosed range: ${JSON.stringify(s)}`);
+  }
+
+  return [{ type: "terminal", value: s.slice(0, end + 1) }, s.slice(end + 1)] as const;
+}
+
+// Group: Ws '(' Seq Ws ')'
+function group(s: string) {
+  let prev = s;
+  if (!s.startsWith("(")) return undefined;
+  s = s.slice(1);
+
+  let value;
+  [value, s] = seq(s);
+
+  s = s.trimStart();
+  if (!s.startsWith(")")) {
+    throw new SyntaxError(`unclosed group: ${JSON.stringify(prev)}`);
+  }
+
+  return [{ type: "group", value }, s.slice(1)] as const;
+}
--- docs/render.ts
+++ docs/render.ts
@@ -1,4 +1,5 @@
 import Slugger, { slug } from "github-slugger";
+import { parseTerms, type Term } from "./lib/grammar.ts";
 import spec from "./spec.yaml" with { type: "yaml" };
 import html from "./spec.html" with { type: "html" };
 
@@ -20,12 +21,11 @@ const BOILERPLATE = `
 `;
 
 type Children = Array<Record<string, unknown> | string>;
-interface Ref {
-  name: string;
-  slug: string;
-  kind: "section";
-  levelStack: number[];
-}
+type Ref =
+  | { kind: "section"; name: string; slug: string; levelStack: number[] }
+  | { kind: "syntax"; name: string; slug: string };
+
+type RefOf<K extends Ref["kind"]> = Extract<Ref, { kind: K }>;
 
 class Renderer {
   readonly #slugger: Slugger = new Slugger();
@@ -89,7 +89,7 @@ class Renderer {
   }
 
   #renderSection(header: string, value: unknown) {
-    let section = this.#findRefByName(header);
+    let section = this.#findRefOf("section", header);
     if (section === undefined) {
       return this.#renderError(`Section not found: '${header}'`);
     }
@@ -101,7 +101,7 @@ class Renderer {
     let dottedLevel = section.levelStack.toReversed().join(".");
     let prefix = this.#isNumbered(section.name) ? `${dottedLevel}&nbsp;` : "";
 
-    this.#output.push(`<section id="${slug}">`);
+    this.#output.push(`<section id="${slug}" class="prose">`);
     this.#output.push(`<${tag}><a href="#${slug}">${prefix}${name}</a></${tag}>`);
 
     if (Array.isArray(value)) {
@@ -121,15 +121,18 @@ class Renderer {
       case "p":
         return this.#renderParagraph(value);
 
+      case "syn":
+        return this.#renderSyntax(value);
+
       default:
         return this.#renderError(`Unknown tag ◊${name}`);
     }
   }
 
   #renderToc() {
-    let sections = this.#refs.filter((s) => s.kind === "section");
+    let sections: Array<RefOf<"section">> = this.#refs.filter((s) => s.kind === "section");
 
-    const childrenOfLevel = (levelStack: number[]): Ref[] => {
+    const childrenOfLevel = (levelStack: number[]) => {
       return sections
         .filter((it) => Bun.deepEquals(levelStack, it.levelStack.slice(1)))
         .filter((it) => this.#isNumbered(it.name));
@@ -171,14 +174,122 @@ class Renderer {
     );
   }
 
-  #renderError(message: string) {
-    this.#output.push(`<p>${errorTag(message)}</p>`);
+  #renderSyntax(value: unknown) {
+    if (!isObject(value)) {
+      this.#renderError("◊syn tag with non-object contents");
+      return;
+    }
+
+    let caption = "Grammar forms";
+    if (typeof value["◊caption"] === "string") {
+      caption += ` for ${value["◊caption"]}`;
+    }
+
+    this.#output.push(`<figure class="syntax">`);
+    this.#output.push(`<dl class="ruleset">`);
+
+    for (let [rule, defs] of Object.entries(value)) {
+      if (rule.startsWith("◊")) continue;
+
+      let ref = this.#findRefOf("syntax", rule);
+      if (ref === undefined) {
+        this.#renderError(`Syntax rule not found: '${rule}'`);
+        continue;
+      }
+
+      this.#output.push(`<div class="rule">`);
+      this.#output.push(
+        `<dt class="rule-name">` +
+          `<a href="#${ref.slug}" class="syntax-nonterminal"><dfn id="${ref.slug}">${rule}</dfn></a>` +
+          `</dt>`,
+      );
+
+      if (!Array.isArray(defs)) {
+        defs = [defs];
+      }
+
+      for (let def of (defs as unknown[]).map(String)) {
+        this.#output.push(`<dd class="rule-def">`);
+
+        if (def.startsWith("◊p")) {
+          this.#output.push(`<span class="syntax-prose">`);
+          this.#output.push(this.#renderInlineTags(def.substring(2).trim(), "syn"));
+        } else {
+          this.#output.push(`<span>`);
+          this.#output.push(this.#renderSyntaxDef(def));
+        }
+
+        this.#output.push(`</span>`);
+        this.#output.push(`</dd>`);
+      }
+
+      this.#output.push("</div>");
+    }
+
+    this.#output.push("</dl>");
+    this.#output.push(`<figcaption class="syntax-caption">${caption}</figcaption>`);
+    this.#output.push("</figure>");
   }
 
-  #renderInlineTags(s: string): string {
-    return s.replaceAll(/◊(\w*)(?:\[(.*?)\])?(?:\[(.*?)\])?/gu, (_, tag, content, extra) =>
-      this.#renderInlineTag(tag, content, extra),
-    );
+  #renderSyntaxDef(def: string): string {
+    let renderTerms: (s: string[], terms: Term[]) => void;
+    let renderTerm: (s: string[], term: Term) => void;
+
+    renderTerms = (s: string[], terms: Term[]) => {
+      s.push(`<span class="syntax-seq">`);
+      for (let term of terms) renderTerm(s, term);
+      s.push(`</span>`);
+    };
+
+    renderTerm = (s: string[], term: Term) => {
+      if (term.quantifier) {
+        s.push(`<span>`);
+      }
+
+      if (term.type === "group") {
+        s.push(`<span class="syntax-parens">(</span>`);
+        renderTerms(s, term.value);
+        s.push(`<span class="syntax-parens">)</span>`);
+      } else if (term.type === "nonterminal") {
+        let ref = this.#findRefOf("syntax", term.value);
+        if (ref === undefined) {
+          s.push(errorTag(`Unknown ref '${term.value}'`));
+          return;
+        }
+
+        s.push(`<a href="#${ref.slug}" class="syntax-${term.type}">${ref.name}</a>`);
+      } else {
+        s.push(`<span class="syntax-${term.type}">${term.value}</span>`);
+      }
+
+      if (term.quantifier) {
+        s.push(`<span class="syntax-quantifier">${term.quantifier}</span>`);
+        s.push(`</span>`);
+      }
+    };
+
+    let terms;
+    try {
+      terms = parseTerms(def);
+    } catch (e) {
+      if (!(e instanceof SyntaxError)) throw e;
+      return errorTag(e.message);
+    }
+
+    let s: string[] = [];
+    renderTerms(s, terms);
+    return s.join("");
+  }
+
+  #renderError(message: string) {
+    this.#output.push(`<p>${errorTag(`Error: ${message}`)}</p>`);
+  }
+
+  #renderInlineTags(s: string, defaultTag: string = "a"): string {
+    return s.replaceAll(/◊(\w*)(?:\[(.*?)\])?(?:\[(.*?)\])?/gu, (_, tag, content, extra) => {
+      if (tag === "" || tag === undefined) tag = defaultTag;
+      return this.#renderInlineTag(tag, content, extra);
+    });
   }
 
   #renderInlineTag(tag: string, content: string | undefined, extra: string | undefined): string {
@@ -186,6 +297,9 @@ class Renderer {
       case "":
       case "a":
         return this.#renderLink(content, extra);
+
+      case "syn":
+        return this.#renderSyntaxLink(content, extra);
 
       case "abbr":
         return this.#renderAbbreviation(content, extra);
@@ -207,14 +321,29 @@ class Renderer {
 
     href ??= body;
 
-    let ref = this.#findRefByName(href);
+    let ref = this.#findRef(href);
     if (ref === undefined) {
-      return errorTag(`Unknown ref '${href}' (slug: '${slug(href)}')`);
+      return errorTag(`◊a: Unknown ref '${href}' (slug: '${slug(href)}')`);
     }
 
     href = `#${ref.slug}`;
 
     return `<a href="${href}">${body}</a>`;
+  }
+
+  #renderSyntaxLink(body: string | undefined, href: string | undefined): string {
+    if (body === undefined) return errorTag("◊syn tag missing body");
+
+    href ??= body;
+
+    let ref = this.#findRefOf("syntax", href);
+    if (ref === undefined) {
+      return errorTag(`◊syn: Unknown ref '${href}' (slug: '${slug(href)}')`);
+    }
+
+    href = `#${ref.slug}`;
+
+    return `<a href="${href}" class="syntax-nonterminal">${body}</a>`;
   }
 
   #renderAbbreviation(body: string | undefined, abbr: string | undefined): string {
@@ -231,25 +360,51 @@ class Renderer {
 
   #renderUnicode(s: string | undefined): string {
     if (s === undefined) return errorTag("◊u tag missing body");
-    return `<i class="unicode">U+${s}</i>`;
+    return `<i class="unicode unicode-emphasis">U+${s}</i>`;
   }
 
   #collectRefs(children: Children, levelStack: number[]) {
     for (let child of children) {
       for (let [key, value] of Object.entries(child)) {
-        if (!key.startsWith("◊") && Array.isArray(value)) {
-          let ref = this.#addRef(key, levelStack);
-          this.#collectRefs(value, [1, ...levelStack]);
-          if (this.#isNumbered(ref.name)) ++levelStack[0];
+        if (key.startsWith("◊")) {
+          if (key === "◊syn" && typeof value === "object" && value !== null) {
+            this.#collectSyntaxRefs(value as Record<string, unknown>);
+          }
+        } else {
+          if (!Array.isArray(value)) continue;
+
+          this.#collectSectionRefs(key, value, levelStack);
         }
       }
     }
   }
 
-  #addRef(name: string, levelStack: number[]): Ref {
+  #collectSyntaxRefs(rules: Record<string, unknown>) {
+    for (let [name, _] of Object.entries(rules)) {
+      this.#addSyntaxRef(name);
+    }
+  }
+
+  #collectSectionRefs(name: string, children: Children, levelStack: number[]) {
+    let ref = this.#addSectionRef(name, levelStack);
+    this.#collectRefs(children, [1, ...levelStack]);
+    if (this.#isNumbered(ref.name)) ++levelStack[0];
+  }
+
+  #findRef(name: string): Ref | undefined {
+    let nameSlug = slug(name);
+    return this.#refs.find((s) => slug(s.name) === nameSlug);
+  }
+
+  #findRefOf<K extends Ref["kind"]>(kind: K, name: string): RefOf<K> | undefined {
+    let nameSlug = slug(name);
+    return this.#refs.find((s) => s.kind === kind && slug(s.name) === nameSlug) as RefOf<K>;
+  }
+
+  #addSectionRef(name: string, levelStack: number[]): Ref {
     let ref: Ref = {
       name,
-      slug: `sec-${this.#slugger.slug(name)}`,
+      slug: this.#slugger.slug(`sec-${name}`),
       kind: "section",
       levelStack: levelStack.slice(),
     };
@@ -257,9 +412,14 @@ class Renderer {
     return ref;
   }
 
-  #findRefByName(name: string): Ref | undefined {
-    let nameSlug = slug(name);
-    return this.#refs.find((s) => slug(s.name) === nameSlug);
+  #addSyntaxRef(name: string): Ref {
+    let ref: Ref = {
+      name,
+      slug: this.#slugger.slug(`syn-${name}`),
+      kind: "syntax",
+    };
+    this.#refs.push(ref);
+    return ref;
   }
 
   #isNumbered(name: string): boolean {
@@ -268,11 +428,15 @@ class Renderer {
 }
 
 function errorTag(message: string): string {
-  return `<strong style="color: red">Error: ${message}</strong>`;
+  return `<strong style="color: red">${message}</strong>`;
 }
 
 function escapeHtml(s: string): string {
   return s.replaceAll("&", "&amp;").replaceAll("<", "&lt;");
+}
+
+function isObject(x: unknown): x is Record<string, unknown> {
+  return typeof x === "object" && x !== null;
 }
 
 async function go() {
