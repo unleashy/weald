f49d9bec489ef16af3d68d198522ea3614056522
bnuuy.bsky.social <me@emmanuelle.dev>
2026-02-19 20:50:32 -0300

    Improve AST

--- Weald.Core/Ast.cs
+++ Weald.Core/Ast.cs
@@ -13,8 +13,9 @@ public interface IAst
     public Loc Loc { get; }
 }
 
-public abstract record Ast
+public sealed record AstName : IAst
 {
+    public required string Value { get; init; }
     public required Loc Loc { get; init; }
 }
 
@@ -22,94 +23,114 @@ public abstract record Ast
 
 public interface IAstExpr : IAst;
 
-public sealed record AstMissing : Ast, IAstExpr;
+public sealed record AstMissing : IAstExpr
+{
+    public required Loc Loc { get; init; }
+}
 
-public sealed record AstTrue : Ast, IAstExpr;
+public sealed record AstTrue : IAstExpr
+{
+    public required Loc Loc { get; init; }
+}
 
-public sealed record AstFalse : Ast, IAstExpr;
+public sealed record AstFalse : IAstExpr
+{
+    public required Loc Loc { get; init; }
+}
 
-public sealed record AstInt : Ast, IAstExpr
+public sealed record AstInt : IAstExpr
 {
     public required Int128 Value { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstFloat : Ast, IAstExpr
+public sealed record AstFloat : IAstExpr
 {
     public required double Value { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstString : Ast, IAstExpr
+public sealed record AstString : IAstExpr
 {
     public required Loc OpeningLoc { get; init; }
     public required string Interpreted { get; init; }
     public required Loc ContentLoc { get; init; }
     public required Loc ClosingLoc { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstVariableRead : Ast, IAstExpr
+public sealed record AstVariableRead : IAstExpr
 {
     public required string Name { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstGroup : Ast, IAstExpr
+public sealed record AstGroup : IAstExpr
 {
     public required Loc OpeningLoc { get; init; }
     public required IAstExpr Body { get; init; }
     public required Loc ClosingLoc { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstBlock : Ast, IAstExpr, IAstStmt
+public sealed record AstBlock : IAstExpr
 {
     public required Loc OpeningLoc { get; init; }
     public required AstStmts Stmts { get; init; }
     public required Loc ClosingLoc { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public interface IAstIfAlternate;
+public interface IAstIfAlternate : IAst;
 
-public sealed record AstIf : Ast, IAstExpr, IAstIfAlternate, IAstStmt
+public sealed record AstIf : IAstExpr, IAstIfAlternate
 {
     public required Loc KwIfLoc { get; init; }
     public required IAstExpr Predicate { get; init; }
     public Loc? TernaryThenLoc { get; init; }
     public required IAstExpr Then { get; init; }
     public IAstIfAlternate? Else { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstElse : Ast, IAstIfAlternate
+public sealed record AstElse : IAstIfAlternate
 {
     public required Loc KwElseLoc { get; init; }
     public required IAstExpr Body { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstAnd : Ast, IAstExpr
+public sealed record AstAnd : IAstExpr
 {
     public required IAstExpr Left { get; init; }
     public required Loc OperatorLoc { get; init; }
     public required IAstExpr Right { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstOr : Ast, IAstExpr
+public sealed record AstOr : IAstExpr
 {
     public required IAstExpr Left { get; init; }
     public required Loc OperatorLoc { get; init; }
     public required IAstExpr Right { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstCall : Ast, IAstExpr
+public sealed record AstCall : IAstExpr
 {
     public required IAstExpr Receiver { get; init; }
     public Loc? OperatorLoc { get; init; }
-    public required string Name { get; init; }
-    public required Loc FunctionLoc { get; init; }
+    public required AstName Function { get; init; }
     public Loc? OpeningLoc { get; init; }
-    public required AstArguments Arguments { get; init; }
+    public AstArguments? Arguments { get; init; }
     public Loc? ClosingLoc { get; init; }
+    public required Loc Loc { get; init; }
 }
 
-public sealed record AstArguments : Ast
+public sealed record AstArguments : IAst
 {
     public required ImmutableArray<IAstExpr> Items { get; init; }
+    public required Loc Loc { get; init; }
 }
 
 #endregion Expressions
@@ -118,19 +139,33 @@ public sealed record AstArguments : Ast
 
 public interface IAstStmt : IAst;
 
-public sealed record AstStmts : Ast, IAst
+public sealed record AstStmtExpr : IAstStmt
+{
+    public required IAstExpr Expr { get; init; }
+    public required Loc Loc { get; init; }
+}
+
+public sealed record AstStmts : IAst
 {
     public required ImmutableArray<IAstStmt> Items { get; init; }
+    public required Loc Loc { get; init; }
 }
 
 public interface IAstDecl : IAstStmt;
 
-public sealed record AstVariableDecl : Ast, IAstDecl
+public sealed record AstVariableDecl : IAstDecl
 {
     public required Loc KwLetLoc { get; init; }
-    public required string Name { get; init; }
+    public required AstName Name { get; init; }
     public required Loc EqLoc { get; init; }
     public required IAstExpr Value { get; init; }
+    public required Loc Loc { get; init; }
 }
 
 #endregion Statements and declarations
+
+public sealed record AstScript : IAst
+{
+    public required AstStmts Stmts { get; init; }
+    public required Loc Loc { get; init; }
+}
